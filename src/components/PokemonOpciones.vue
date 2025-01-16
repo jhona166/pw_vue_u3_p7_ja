e58719439ebfeaeffe991c4d63a19af91c2cad1e
jhona166 <jdaltamiranoo@uce.edu.ec>
2025-01-15 19:50:12 -0500

    Taller21

---
+++ src/components/PokemonOpciones.vue
@@ -0,0 +1,13 @@
+<template>
+  <p1>Pokemon Opciones</p1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/page/PokemonPage.vue
@@ -0,0 +1,26 @@
+<template>
+   <h2> Adivina el pokemon de la Imagebn</h2>
+   <PokemonImagen/>
+   <PokemonOpciones/>
+</template>
+
+<script>
+import PokemonImagen from "../components/PokemonImagen.vue";
+import PokemonOpciones from "../components/PokemonOpciones.vue";
+
+
+export default {
+    components:{
+        PokemonImagen,
+        PokemonOpciones
+    },
+    props:{
+       
+    }
+
+}
+</script>
+
+<style>
+
+</style>
